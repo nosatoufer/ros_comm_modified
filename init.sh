885dabdef98e391f519a3a08959dc93ca4160468
nosatoufer <gdufour05@gmail.com>
2019-04-11 15:20:34 +0200

    fixed script

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 #Pull from the repo and move the necessary files into the ros installation
 
 git pull
-cp -r ./ros_comm_modified/dist-packages/* /opt/ros/melodic/lib/python2.7/dist-packages
+cp -r ./dist-packages/* /opt/ros/melodic/lib/python2.7/dist-packages
