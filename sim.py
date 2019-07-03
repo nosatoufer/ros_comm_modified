2c4eb4606f46f4c0245d358f4d08d59e64e59970
nosatoufer <gdufour05@gmail.com>
2019-07-03 21:18:37 +0200

    fix sim

--- sim.py
+++ sim.py
@@ -1,70 +1,101 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
+import sys
+import socket
+
 from std_msgs.msg import String
 
-#class Node():
-class Master():#Node):
+node = None
 
-    
-    def __init__(name="test")::
-        self.name = "%s%d"%(name, math.rand())
+def handleSys(data):
+    msg = data.data
+    words = msg.split(" ")
+    print(rospy.get_caller_id())
+    if  words[0] == "oob":
+        name = words[1]
+        node.rmOccupation(name)
+        node.addNode(name)
+    elif words[0] == "moved":
+        name = words[1]
+        loc = words[2]
+        node.addOccupation(name, loc)
+    elif words[0] == "newMaster":
+        uri = words[1]
+        node.master = uri
+    elif words[0] == "newNode":
+        node.addNode(words[1])
+    elif words[0] == "attribute" and words[1] == node.name:
+        node.move(words[2])
+    else:
+        pass
+
+def handle(data):
+    words = data.data.split(" ")
+    print(words)
+    if words[0] == "attribute":
+        node.move(words[1])
+
+class Master():
+
+
+    def __init__(self, name="test"):
+        self.name = name
         rospy.init_node(name, anonymous=True)
-        self.pubSys = rospy.Publisher('system', String, queue_size=10)
-        self.subSys = rospy.Subscriber('system', String, handleSys)
-        self.pubPr = rospy.Publisher('%s/move'%self.name, String, queue_size=10)
-        self.subPr = rospy.Publisher('%s/move'%self.name, String, handle)
+        self.pubSys = rospy.Publisher('coordination', String, queue_size=10)
+        self.subSys = rospy.Subscriber('coordination', String, handleSys)
+        #self.pubPr = rospy.Publisher('%s/move'%self.name, String, queue_size=10)
+        #self.subPr = rospy.Subscriber('%s/move'%self.name, String, handle)
         self.battery = 100
         self.charging = {}
-        self.full = {}
         self.occupation = {}
+        self.slots = 3
         self.master = None
         self.charger = (0,0)
+        self.pubSys.publish("newNode %s" %(name))
 
-    def handle(data):
-        msg = data.data
-        words = data.split(' ')
-        if words[0] == "attribute":
+    def addNode(self, node):
+        self.charging[node] = 100
 
+    def rmNode(self, node):
+        del self.charging[node]
 
-    def handleSys(data):
-        msg = data.data
-        words = data.split(' ')
-        if  words[0] == "oob":
-            self.occupation.remove(data.caller_id)
-        elif words[0] == "moved":
-            self.occupation.add(pair(data.caller_id,words[1]))
-        elif words[0] == "newMaster":
-            self.master = words[1]
-        else:
-            pass
+    def addOccupation(self, node, location):
+        del self.charging[node]
+        self.occupation[node] = location
 
-    def move(location):
-        # move to location
-        #
+    def rmOccupation(self, node):
+        del self.occupation[node]
+        self.charging[node] = 100
 
-    def recharge():
-        self.pubSys.publish("oob")
+    def move(self, location):
+        pubSys.publish("moved %s %s" %(self.name, location))
+
+    def recharge(self):
+        self.pubSys.publish("oob %s" % self.name)
         move(self.charger)
 
-    def startMaster():
-        
+    def startMaster(self):
+        i = 0
+        while len(self.occupation) < self.slots - 1 and len(self.charging) > 0 :
+            attribute(i)
 
-    def setMaster(uri):
+    def setMaster(self, uri):
         self.master = uri
 
-    def attribute(slot):
-        if(!self.full.empty()):
-            node = self.full.pop()
-        else:
-            node = self.charging.pop()
-        
-        node.publish("attribute %s" % slot)
+    def attribute(self, slot):
+        if len(self.charging) > 0:
+            for i, v in self.charging.items():
+                if v > 0:
+                    self.pubSys.publish("attribute %s %s" % (i,slot))
 
 
 if __name__ == '__main__':
     try:
-        node = Master()
+        name = socket.gethostname()
+        print(name)
+        node = Master(name)
+        rospy.spin()
 
     except rospy.ROSInterruptException:
         pass
--- talker.py
+++ talker.py
@@ -4,9 +4,8 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    pub2 = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('coordination', String, queue_size=10)
+    rospy.init_node('talker', anonymous=False)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
